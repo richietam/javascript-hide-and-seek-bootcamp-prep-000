5f572df853187fd76c5edd3f67d6a0abba8121eb
richardtam.ny <richardtam.ny@gmail.com>
2018-12-12 05:33:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 
+function getFirstSelector(selector) {
+  var x = document.querySelector(selector);
+  console.log(x);
+  return x
+}
+
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  var i = 0
+  for(i=0; i < lis.length; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML)+ n;
+  }
+}
+
+function deepestChild() {
+  var lis = document.getElementById("grand-node").querySelectorAll("div");
+  var test;
+  var i = 0;
+  for(i=0; i < lis.length-1; i++) {
+    test = lis[i].querySelector("div");
+    return test;
+  }
+}
